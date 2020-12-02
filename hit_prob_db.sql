46c3f76d463d82267a7e909b6562ac0d26aa805d
jerrymckennan <74614356+jerrymckennan@users.noreply.github.com>
2020-12-02 10:42:00 -0500

    Update hit_prob_db.sql

--- hit_prob_db.sql
+++ hit_prob_db.sql
@@ -1,14 +1,12 @@
 /*
 
-This script can be run within MySQL for the first time setting up or for reloading all data.
-It will check to see if the database and table have been created or not, then do so if they
-haven't. Then it will delete any data that might be in there just to ensure there is no
-duplicated data. From there it will take each of the CSV files that was created with the
-Python script. Each section can be removed to accommodate the number of CSV files you want
-to use. You can also copy and paste to add more, if you want.
+This script can be run within MySQL for the first time setting up any or all databases. 
+It will check to see if the database and table have been created or not, then do so if they haven't.
+If this is not the first time using it or you want a clean set up, use the delete_data.sql script as well. That will delete all data located in all of these tables. If you want to add data, use the add_data.sql script. 
 
-I enjoy using the SQL script to handle this, seems faster. Though I will try to also make a
-script for Python for those who want to use that as well.
+I personally like to use the command line, change directory to where this script is located and issuing the following command:
+
+	mysql -u username -p < name_you_gave_this_file.sql
 
 */
 
@@ -34,141 +32,46 @@ year SMALLINT,
 ev SMALLINT
 );
 
-DELETE FROM prob;
-
-LOAD DATA INFILE '/directory/file.csv' INTO TABLE prob
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-( 
-hits_1b_per_hip,
-hits_2b_per_hip,
-hits_3b_per_hip,
-hits_hr_per_hip,
-hits_per_hip,
-n_hip,
-n_hits,
-n_hits_1b,
-n_hits_2b,
-n_hits_3b,
-n_hits_hr,
-launch_angle,
-woba,
-year,
-ev
+CREATE TABLE IF NOT EXISTS fg_park_factor (
+season SMALLINT,
+team varchar(25),
+basic_5yr SMALLINT,
+3yr SMALLINT,
+1yr SMALLINT,
+1b SMALLINT,
+2b SMALLINT,
+3b SMALLINT,
+hr SMALLINT,
+so SMALLINT,
+bb SMALLINT,
+gb SMALLINT,
+fb SMALLINT,
+ld SMALLINT,
+iffb SMALLINT,
+fip SMALLINT
 );
 
-
-LOAD DATA INFILE '/directory/file.csv' INTO TABLE prob
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-( 
-hits_1b_per_hip,
-hits_2b_per_hip,
-hits_3b_per_hip,
-hits_hr_per_hip,
-hits_per_hip,
-n_hip,
-n_hits,
-n_hits_1b,
-n_hits_2b,
-n_hits_3b,
-n_hits_hr,
-launch_angle,
-woba,
-year,
-ev
+CREATE TABLE IF NOT EXISTS team_names (
+city_state varchar(25),
+nickname varchar(25),
+3_letter_name varchar(3)
 );
 
-
-LOAD DATA INFILE '/directory/file.csv' INTO TABLE prob
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-( 
-hits_1b_per_hip,
-hits_2b_per_hip,
-hits_3b_per_hip,
-hits_hr_per_hip,
-hits_per_hip,
-n_hip,
-n_hits,
-n_hits_1b,
-n_hits_2b,
-n_hits_3b,
-n_hits_hr,
-launch_angle,
-woba,
-year,
-ev
-);
-
-
-LOAD DATA INFILE '/directory/file.csv' INTO TABLE prob
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-( 
-hits_1b_per_hip,
-hits_2b_per_hip,
-hits_3b_per_hip,
-hits_hr_per_hip,
-hits_per_hip,
-n_hip,
-n_hits,
-n_hits_1b,
-n_hits_2b,
-n_hits_3b,
-n_hits_hr,
-launch_angle,
-woba,
-year,
-ev
-);
-
-
-LOAD DATA INFILE '/directory/file.csv' INTO TABLE prob
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-( 
-hits_1b_per_hip,
-hits_2b_per_hip,
-hits_3b_per_hip,
-hits_hr_per_hip,
-hits_per_hip,
-n_hip,
-n_hits,
-n_hits_1b,
-n_hits_2b,
-n_hits_3b,
-n_hits_hr,
-launch_angle,
-woba,
-year,
-ev
-);
-
-
-LOAD DATA INFILE '/directory/file.csv' INTO TABLE prob
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-( 
-hits_1b_per_hip,
-hits_2b_per_hip,
-hits_3b_per_hip,
-hits_hr_per_hip,
-hits_per_hip,
-n_hip,
-n_hits,
-n_hits_1b,
-n_hits_2b,
-n_hits_3b,
-n_hits_hr,
-launch_angle,
-woba,
-year,
-ev
+CREATE TABLE IF NOT EXISTS player (
+player_name varchar(50),
+at_bat TINYINT,
+ab_num TINYINT,
+bb_walk TINYINT,
+ball_in_play TINYINT,
+distance DECIMAL(5,2),
+ev SMALLINT,
+event varchar(25),
+eventPretty varchar(25),
+game_date DATE,
+hit TINYINT,
+is_1b TINYINT,
+is_2b TINYINT,
+is_3b TINYINT,
+is_hr TINYINT,
+launch_angle TINYINT
 );

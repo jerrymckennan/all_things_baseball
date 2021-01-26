6202aec1778edeef299eead45eeeb95a8802f4ee
jerrymckennan <74614356+jerrymckennan@users.noreply.github.com>
2021-01-26 15:17:25 -0500

    Update plt_heatmap_player.py

--- graphing/plt_heatmap_player.py
+++ graphing/plt_heatmap_player.py
@@ -43,8 +43,8 @@ while True:
             print("Error with password, re-enter:")
             mysql_pass = getpass.getpass()
     finally:
-        connection = pymysql.connect(host='localhost',
-                         user='root',
+        connection = pymysql.connect(host='',
+                         user='',
                          password=mysql_pass,
                          db='hit_probability',
                          charset='utf8mb4',
